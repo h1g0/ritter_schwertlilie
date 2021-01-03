12d2c154bebead8bb94e54e9817e94124d81373a
hi_go <me@clutte.red>
2021-01-03 15:48:03 +0900

    Change game engine to DxLib.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,43 @@
-extern crate piston_window;
-use piston_window::*;
+extern crate encoding_rs;
+use encoding_rs::*;
+extern crate rust_dxlib;
+use rust_dxlib::*;
 
+/*
 mod input_state;
 use input_state::*;
 
 mod player_ship;
 use player_ship::*;
 
+*/
+
 ///ウィンドウタイトル
-const WINDOW_TITLE: &str = "ritter_schwertlilie";
+const WINDOW_TITLE: &str = "ritter_schwertlilie ver. 0.1.0 alpha";
 ///画面サイズ
-const WINDOW_SIZE: Size = Size {
-    width: 640.0,
-    height: 480.0,
-};
+const WINDOW_SIZE: (i32,i32) = (640,480);
+const COLOR_BIT: i32 = 32;
+const REFRESH_RATE: i32 = 60;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new(WINDOW_TITLE, WINDOW_SIZE)
-        .exit_on_esc(true)
-        .vsync(true)
-        .resizable(false)
-        .samples(4)
-        .build()
-        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
-    window.events.set_max_fps(60);
-    window.events.set_ups(60);
 
-    let mut input_state = InputState::new();
-    let mut player = PlayerShip::new(
-        Size {
-            width: WINDOW_SIZE.width,
-            height: WINDOW_SIZE.height,
-        },
-        Texture::from_path(
-            &mut window.create_texture_context(),
-            "img/player.png",
-            Flip::None,
-            &TextureSettings::new())
-            .unwrap(),
-        Size {
-            width: 64.0,
-            height: 64.0,
-        },
-        Size {
-            width: 2.0,
-            height: 2.0,
-        },
-        320.0,
-        240.0,
-        3,
-    );
-    while let Some(e) = window.next() {
-        match e {
-            Event::Loop(Loop::Render(_)) => {
-                window.draw_2d(&e, |c, g, _| {
-                    clear([0.0, 0.0, 0.0, 1.0], g);
-                    player.draw(c, g);
-                });
-            }
-            Event::Loop(Loop::Update(_)) => {
-                player.move_by_input(&input_state);
-            }
-            Event::Input(i, _) => {
-                if let Input::Button(key) = i {
-                    input_state.set(&key);
-                }
-            }
-            _ => {}
+    unsafe {
+
+        dx_ChangeWindowMode(TRUE);
+        dx_SetGraphMode(WINDOW_SIZE.0, WINDOW_SIZE.1, COLOR_BIT, REFRESH_RATE);
+        dx_DxLib_Init();
+        dx_SetDrawScreen(DX_SCREEN_BACK);
+        /*
+        let (sjis_str,_,_) = SHIFT_JIS.encode(WINDOW_TITLE);
+        let title = sjis_str.as_ptr();
+        dx_SetMainWindowText(title);
+        */
+        dx_SetMainWindowText(WINDOW_TITLE.as_ptr());
+
+        // メインループ( 裏画面を表画面に反映, メッセージ処理, 画面クリア )
+        while dx_ScreenFlip() == 0 && dx_ProcessMessage() == 0 && dx_ClearDrawScreen() == 0 {
+            
         }
+        dx_DxLib_End();
     }
 }
