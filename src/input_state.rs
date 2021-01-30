40b41406ac595c033255447fbb6fbadc95196774
hi_go <me@clutte.red>
2021-01-30 23:38:25 +0900

    WIP: Implement `InputState`.

--- src/input_state.rs
+++ src/input_state.rs
@@ -1,5 +1,10 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 extern crate rust_dxlib;
 use rust_dxlib::*;
+
+type KeyStateBuff = [c_char; 256];
 
 pub struct InputState {
     pub up: bool,
@@ -24,6 +29,59 @@ impl InputState {
             shift: false,
         };
     }
+
+    pub fn get_key_state() -> KeyStateBuff {
+        unsafe {
+            let mut buf: KeyStateBuff;
+            dx_GetHitKeyStateAll(buf);
+            return buf;
+        }
+    }
+
+    pub fn set(&mut self, buffer: &KeyStateBuff) {
+        self.up = if buffer[KEY_INPUT_UP as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.down = if buffer[KEY_INPUT_DOWN as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.left = if buffer[KEY_INPUT_LEFT as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.right = if buffer[KEY_INPUT_RIGHT as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.z = if buffer[KEY_INPUT_Z as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.x = if buffer[KEY_INPUT_X as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.c = if buffer[KEY_INPUT_C as usize] == 0 {
+            false
+        } else {
+            true
+        };
+        self.shift = if buffer[KEY_INPUT_LSHIFT as usize] == 0 {
+            false
+        } else {
+            true
+        };
+
+    }
+
     /*
     pub fn set(&mut self, key: &ButtonArgs) {
         match key.button {
