7a645ecb91f8fc318753cbd0484c88ebe19bb3bb
hi_go <me@clutte.red>
2021-02-09 02:07:17 +0900

    Refactor.

--- src/background.rs
+++ src/background.rs
@@ -1,16 +1,19 @@
-pub struct BackGround{
-    field_size: (u32,u32),
-    show_size: (u32,u32),
-    center_point: (u32,u32),
+pub struct Background {
+    field_size: (u32, u32),
+    show_size: (u32, u32),
+    center_point: (u32, u32),
 }
 
-impl BackGround{
-    pub fn new(field_size:(u32,u32),show_size:(u32,u32),center_point:(u32,u32))->BackGround{
-        BackGround{
+impl Background {
+    pub fn new(
+        field_size: (u32, u32),
+        show_size: (u32, u32),
+        center_point: (u32, u32),
+    ) -> Background {
+        Background {
             field_size,
             show_size,
             center_point,
         }
     }
-    
 }
