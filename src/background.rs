33dfddd69bcb532cd0ea055fc39ca0e5ff088a3a
hi_go <me@clutte.red>
2021-02-17 22:14:07 +0900

    WIP: Implement `Background::get_show_rect`.

--- src/background.rs
+++ src/background.rs
@@ -1,4 +1,5 @@
 use crate::math::vec2d::*;
+use crate::math::rectangle::*;
 
 pub struct Background {
     field_size: Vec2d<u32>,
@@ -19,6 +20,29 @@ impl Background {
         }
     }
 
+    pub fn get_show_rect(&mut self)->Rectangle{
+        let half_show_x: i32 = (self.show_size.x / 2) as i32;
+        let half_show_y: i32 = (self.show_size.y / 2) as i32;
+        Rectangle::new(
+            Vec2d::<i32>::new(
+                // TODO: x,yがfield_sizeを超える時の処理
+            if self.center_point.x > half_show_x{
+                if self.center_point.x + half_show_x > self.field_size.x{
+                    self.
+                }
+                self.center_point.x - half_show_x
+            }else{
+                0
+            },
+            if self.center_point.y > half_show_y{
+                self.center_point.y - half_show_y
+            }else{
+                0
+            }),
+            Vec2d::<i32>::new(self.field_size.x as i32,self.field_size.y as i32)
+        )
+        
+    }
     pub fn set_center_point(&mut self, p: Vec2d<i32>) {
         self.center_point = p;
     }
